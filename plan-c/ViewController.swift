c2e192807695a265f46d47670ecdd1472b30abb1
Christine <bumfuzzelz@gmail.com>
2022-08-02 11:19:13 -0700

    Initial Commit

---
+++ plan-c/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  plan-c
+//
+//  Created by Christine Zhang on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
