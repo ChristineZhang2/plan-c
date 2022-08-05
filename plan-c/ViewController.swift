8cd606579deef1a75f436abc02f5e3bd2c75dd39
Priscilla Espinoza <priscillaespinoza6@icloud.com>
2022-08-05 07:24:00 -0700

    done

--- plan-c/ViewController.swift
+++ plan-c/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signInButton: UIButton!
+   
+    var backGroundPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func playBackGroundMusic (fileNamed: String){
+        
+        let url = Bundle.main.url(forResource: fileNamed, withExtension: nil)
+        guard let newUrl = url else {
+            
+            print("Could not find filed called \(fileNamed)")
+            return
+        }
+        do {
+            
+            backGroundPlayer = try AVAudioPlayer(contentsOf: newUrl)
+            backGroundPlayer.numberOfLoops = -1
+            backGroundPlayer.prepareToPlay()
+            backGroundPlayer.play()
+        }
+        catch let error as NSError {
+            
+            print(error.description)
+        }
+    }
+    
+    
     @IBAction func buttonPressed(_ sender: UIButton) {
         
         if sender.isSelected {
@@ -21,6 +48,7 @@ class ViewController: UIViewController {
         } else {
             sender.isSelected = true
         }
+        playBackGroundMusic(fileNamed: "Lofi.m4a")
     
 }
 
